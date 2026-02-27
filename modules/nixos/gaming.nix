587198f733136214715289bab6c48dc7edd6abe6
Noah Kamara <mail@noahkamara.com>
2026-02-28 00:18:43 +0100

    feat: configure steam

--- modules/nixos/gaming.nix
+++ modules/nixos/gaming.nix
@@ -1,7 +1,11 @@
 { pkgs, ... }:
 
 {
-  programs.steam.enable = true;
+  programs.steam = {
+    enable = true;
+    extest.enable = true;
+  };
+
 
   environment.systemPackages = with pkgs; [
     discord
