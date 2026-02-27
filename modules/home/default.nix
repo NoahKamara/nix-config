5d1ea5061a20ae74d4edcec70bb86597f99ea035
Noah Kamara <mail@noahkamara.com>
2026-02-27 23:42:51 +0100

    feat: configure hyprland

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -87,149 +87,24 @@
   programs.zsh.enable = true;
   programs.fish.enable = true;
 
+  home.pointerCursor = lib.mkIf pkgs.stdenv.isLinux {
+    package = pkgs.adwaita-icon-theme;
+    name = "Adwaita";
+    size = 24;
+    gtk.enable = true;
+    x11.enable = true;
+  };
+
   wayland.windowManager.hyprland = lib.mkIf pkgs.stdenv.isLinux {
     enable = true;
     systemd.enable = true;
-    settings = {
-      "$mainMod" = "SUPER";
-
-      exec-once = [
-        "waybar"
-        "mako"
-      ];
-
-      general = {
-        gaps_in = 5;
-        gaps_out = 10;
-        border_size = 2;
-        layout = "dwindle";
-      };
-
-      input = {
-        follow_mouse = 1;
-        touchpad.natural_scroll = true;
-      };
-
-      decoration = {
-        rounding = 8;
-        blur = {
-          enabled = true;
-          size = 3;
-          passes = 1;
-        };
-      };
-
-      animations.enabled = true;
-
-      dwindle = {
-        pseudotile = true;
-        preserve_split = true;
-      };
-
-      bind = [
-        "$mainMod, Return, exec, ghostty"
-        "$mainMod, D, exec, wofi --show drun"
-        "$mainMod, Q, killactive,"
-        "$mainMod SHIFT, E, exit,"
-        "$mainMod, V, togglefloating,"
-        "$mainMod, F, fullscreen,"
-        "$mainMod, L, exec, hyprlock"
-
-        "$mainMod, left, movefocus, l"
-        "$mainMod, right, movefocus, r"
-        "$mainMod, up, movefocus, u"
-        "$mainMod, down, movefocus, d"
-
-        "$mainMod, 1, workspace, 1"
-        "$mainMod, 2, workspace, 2"
-        "$mainMod, 3, workspace, 3"
-        "$mainMod, 4, workspace, 4"
-        "$mainMod, 5, workspace, 5"
-        "$mainMod, 6, workspace, 6"
-        "$mainMod, 7, workspace, 7"
-        "$mainMod, 8, workspace, 8"
-        "$mainMod, 9, workspace, 9"
-        "$mainMod, 0, workspace, 10"
-
-        "$mainMod SHIFT, 1, movetoworkspace, 1"
-        "$mainMod SHIFT, 2, movetoworkspace, 2"
-        "$mainMod SHIFT, 3, movetoworkspace, 3"
-        "$mainMod SHIFT, 4, movetoworkspace, 4"
-        "$mainMod SHIFT, 5, movetoworkspace, 5"
-        "$mainMod SHIFT, 6, movetoworkspace, 6"
-        "$mainMod SHIFT, 7, movetoworkspace, 7"
-        "$mainMod SHIFT, 8, movetoworkspace, 8"
-        "$mainMod SHIFT, 9, movetoworkspace, 9"
-        "$mainMod SHIFT, 0, movetoworkspace, 10"
-
-        ", Print, exec, grim -g \"$(slurp)\" - | wl-copy"
-      ];
-
-      bindm = [
-        "$mainMod, mouse:272, movewindow"
-        "$mainMod, mouse:273, resizewindow"
-      ];
-
-      bindl = [
-        ", XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
-      ];
-
-      bindle = [
-        ", XF86AudioRaiseVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+"
-        ", XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
-        ", XF86MonBrightnessUp, exec, brightnessctl set +5%"
-        ", XF86MonBrightnessDown, exec, brightnessctl set 5%-"
-      ];
-    };
-  };
-
-  programs.hyprlock = lib.mkIf pkgs.stdenv.isLinux {
-    enable = true;
-    settings = {
-      general = {
-        hide_cursor = true;
-        grace = 5;
-      };
-      background = [{
-        path = "screenshot";
-        blur_passes = 3;
-        blur_size = 8;
-      }];
-      input-field = [{
-        size = "200, 50";
-        position = "0, -80";
-        monitor = "";
-        dots_center = true;
-        fade_on_empty = false;
-        outline_thickness = 2;
-        placeholder_text = "Password...";
-      }];
-    };
-  };
-
-  services.hypridle = lib.mkIf pkgs.stdenv.isLinux {
-    enable = true;
-    settings = {
-      general = {
-        lock_cmd = "pidof hyprlock || hyprlock";
-        before_sleep_cmd = "loginctl lock-session";
-        after_sleep_cmd = "hyprctl dispatch dpms on";
-      };
-      listener = [
-        {
-          timeout = 300;
-          on-timeout = "loginctl lock-session";
-        }
-        {
-          timeout = 600;
-          on-timeout = "hyprctl dispatch dpms off";
-          on-resume = "hyprctl dispatch dpms on";
-        }
-      ];
-    };
   };
 
   home.sessionVariables = lib.mkIf pkgs.stdenv.isLinux {
+    HYPRCURSOR_THEME = "Adwaita";
+    HYPRCURSOR_SIZE = "24";
+    XCURSOR_THEME = "Adwaita";
+    XCURSOR_SIZE = "24";
     NIXOS_OZONE_WL = "1";
     MOZ_ENABLE_WAYLAND = "1";
   };
