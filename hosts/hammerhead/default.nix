ba88be7e7405d388182b5500e6bd633be491f465
Noah Kamara <mail@noahkamara.com>
2026-02-27 20:21:59 +0100

    chore(hammerhead): use var for username

--- hosts/hammerhead/default.nix
+++ hosts/hammerhead/default.nix
@@ -1,6 +1,7 @@
 { self, inputs, pkgs, ... }:
-
-{
+let 
+  username = "noah";
+in {
   imports = [
     ../../modules/shared
     ../../modules/darwin
@@ -10,9 +11,9 @@
   programs.fish.enable = true;
   environment.shells = [ pkgs.fish ];
 
-  users.users.noahkamara = {
-    name = "noahkamara";
-    home = "/Users/noahkamara";
+  users.users.${username} = {
+    name = username;
+    home = "/Users/${username}";
     shell = pkgs.fish;
   };
 
@@ -20,17 +21,17 @@
     useGlobalPkgs = true;
     useUserPackages = true;
     backupFileExtension = "backup";
-    users.noahkamara = import ../../modules/home;
+    users.${username} = import ../../modules/home;
   };
 
-  system.primaryUser = "noahkamara";
+  system.primaryUser = username;
 
   # nix-darwin does not change the shell for existing users by default
   # so we use an activation script to enforce it declaratively.
   system.activationScripts.postActivation.text = ''
-    if [ "$(/usr/bin/dscl . -read /Users/noahkamara UserShell | awk '{print $2}')" != "/run/current-system/sw/bin/fish" ]; then
+    if [ "$(/usr/bin/dscl . -read /Users/${username} UserShell | awk '{print $2}')" != "/run/current-system/sw/bin/fish" ]; then
       echo "Changing user shell to fish..."
-      chsh -s /run/current-system/sw/bin/fish noahkamara
+      chsh -s /run/current-system/sw/bin/fish ${username}
     fi
   '';
 
