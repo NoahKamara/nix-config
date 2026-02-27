6b83be580ecd431a249f5474ca8316a0c8a2559e
Noah Kamara <mail@noahkamara.com>
2026-02-28 00:19:21 +0100

    Install comfyui via nix

--- flake.nix
+++ flake.nix
@@ -8,7 +8,6 @@
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     comfyui-nix.url = "github:utensils/comfyui-nix/8b2a35890823c8529a25a57c4d9fdbd712aa3b38";
-    comfyui-nix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { self, nix-darwin, nixpkgs, home-manager, comfyui-nix, ... } @ inputs:
