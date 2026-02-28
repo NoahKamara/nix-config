53180200d92b3f7bb359633695bb3eed4b6133c8
Noah Kamara <mail@noahkamara.com>
2026-02-28 15:49:53 +0000

    chore: update hardware config

--- hosts/nebulon/hardware-configuration.nix
+++ hosts/nebulon/hardware-configuration.nix
@@ -8,8 +8,8 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "uas" "usb_storage" "sd_mod" ];
-  boot.initrd.kernelModules = [ ];
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "uas" "sd_mod" ];
+  boot.initrd.kernelModules = [ "dm-snapshot" ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
