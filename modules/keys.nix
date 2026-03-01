f2358368f6c368ddb29c75213a691017fd5d18c1
Noah Kamara <mail@noahkamara.com>
2026-03-01 22:44:58 +0100

    fix(nebulon): update SSH key for nebulon user

--- modules/keys.nix
+++ modules/keys.nix
@@ -1,4 +1,4 @@
 {
   hammerhead = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC4I9WnP4CxyreDlKM3sMS2M6hpShdRsqQpWerj9D4cB noahkamara@hammerhead";
-  nebulon = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOgdCnY9CjbDVmajWXsXYRsFpS5wDk2c5DH/ZZbseoHy mail@noahkamara.com";
+  nebulon = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOgdCnY9CjbDVmajWXsXYRsFpS5wDk2c5DH/ZZbseoHy noah@nebulon";
 }
