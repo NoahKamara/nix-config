dc018364ff012411a7e34f201ab4a713b4a3e6b9
Noah Kamara <mail@noahkamara.com>
2026-03-09 23:59:42 +0100

    feat(shared): add ai.cachix.org as trusted substitutor

--- modules/shared/default.nix
+++ modules/shared/default.nix
@@ -21,6 +21,9 @@
       "https://ai.cachix.org"
       "https://nix-community.cachix.org"
     ];
+    trusted-substituters = [
+      "https://ai.cachix.org"
+    ];
     trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "ai.cachix.org-1:N9dzRK+alWwoKXQlnn0H6aUx0lU/mspIoz8hMvGvbbc="
